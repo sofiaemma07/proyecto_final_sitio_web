b27d037344c28a86dbbe6eceaa35e85ae03371d4
felipe <felipeigarzabal@gmail.com>
2025-11-02 16:44:28 -0300

    avances de pipe

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 
 
 // Espera a que todo el contenido del DOM esté cargado
+
 document.addEventListener('DOMContentLoaded', function() {
     
 
@@ -245,8 +246,64 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+const contactForm = document.getElementById("contactForm");
+if (contactForm) {
+    contactForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const contacto = {
+            nombre: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            mensaje: document.getElementById("message").value
+        };
+
+        let contactos = JSON.parse(localStorage.getItem("contactos")) || [];
+        contactos.push(contacto);
+        localStorage.setItem("contactos", JSON.stringify(contactos));
+
+        const lang = document.getElementById('language').value;
+        if (lang === 'en') {
+            alert("Your message has been sent successfully.");
+        } else {
+            alert("El mensaje se ha enviado con éxito.");
+        }
+    });
+}
+
+
 
     });
+function chequearDiv() {
+    const productos = document.querySelectorAll('.product-card'); // todos los productos
+    const inputMax = document.getElementById('max-price');
+    const precio_maximo = parseFloat(inputMax.value); // lo que puso el usuario
+
+    const contenedor = document.getElementById('resultado');
+
+    // Borrar cualquier mensaje previo
+    contenedor.innerHTML = '';
+
+    let hayVisible = false; // bandera para saber si hay algún producto que cumpla
+
+    productos.forEach(producto => {
+        const precioTexto = producto.querySelector('.product-price').textContent; // "$12500"
+        const precio = parseFloat(precioTexto.replace('$', '').replace(',', '')); // número
+
+        if (precio > precio_maximo) {
+            producto.style.display = 'none';
+        } else {
+            producto.style.display = 'block';
+            hayVisible = true; // hay al menos un producto que cumple
+        }
+    });
+
+    // Si no hay productos visibles, mostrar mensaje
+    if (!hayVisible) {
+        const p = document.createElement('p');
+        p.textContent = "No hay objetos que cumplan con sus requerimientos";
+        contenedor.appendChild(p);
+    }
+}
 
 
 const menuToggle = document.getElementById('menu-toggle');
@@ -255,6 +312,8 @@ const mainNav = document.getElementById('main-nav');
 menuToggle.addEventListener('click', () => {
     mainNav.classList.toggle('active');
 });
+
+
 
 
 /* NOTAS Y ACLARACIONES
